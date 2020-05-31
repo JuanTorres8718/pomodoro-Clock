8dd6759bbd967c541ff33bb8e29a58f23c48b3e8
JuanTorres8718 <jdtorres8718@gmail.com>
2020-05-31 11:52:34 -0500

    Updates

---
+++ precache-manifest.1bc4335eb66239ee3fdb8c8dee076ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d0c81e9363bb15796e11f73e7c5738",
+    "url": "/pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "2512edd7fd1e59708b15",
+    "url": "/pomodoro-Clock/static/css/main.555dd4ca.chunk.css"
+  },
+  {
+    "revision": "c1a7f60212c3eb148ca0",
+    "url": "/pomodoro-Clock/static/js/2.292b4bde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-Clock/static/js/2.292b4bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2512edd7fd1e59708b15",
+    "url": "/pomodoro-Clock/static/js/main.32ad67da.chunk.js"
+  },
+  {
+    "revision": "f13efd6a5b3e038c7cd3",
+    "url": "/pomodoro-Clock/static/js/runtime-main.524a090b.js"
+  }
+]);
